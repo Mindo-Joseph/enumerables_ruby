c24de79f15d4e7028c2d944915354a72da00acbb
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-04-14 23:34:46 +0300

    create my_select method and test to see if behaves same as select

--- enumerables.rb
+++ enumerables.rb
@@ -2,8 +2,7 @@ module Enumerable
   def my_each
     position = 0
     return to_enum unless block_given?
-
-    while position <= length
+    while position <= length do
       yield self[position]
       position += 1
     end
@@ -13,11 +12,23 @@ module Enumerable
   def my_each_with_index
     position = 0
     return to_enum unless block_given?
-
-    while position <= length
-      yield self[position], position
-      position += 1
+    while position < length do
+      yield self[position],position
+      position +=1
     end
     self
   end
+  # Define my_select
+  def my_select
+    result = []
+    return to_enum unless block_given?
+
+    my_each do |item|
+      result << item if yield item
+    end
+    result
+  end
+
 end
+print [1,2,3,5].my_select.class == [1,2,3,4,5].select.class
+puts 
