6fb713ee6e4eb5327ff2c6f35eafbd73d668903b
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-04-14 23:22:48 +0300

    define my_each_with_index

--- enumerables.rb
+++ enumerables.rb
@@ -2,13 +2,22 @@ module Enumerable
   def my_each
     position = 0
     return to_enum unless block_given?
-    while position <= length do
+
+    while position <= length
       yield self[position]
       position += 1
     end
     self
   end
 
+  def my_each_with_index
+    position = 0
+    return to_enum unless block_given?
+
+    while position <= length
+      yield self[position], position
+      position += 1
+    end
+    self
+  end
 end
-[1,2,3,5].my_each { |i| print i.to_s + " "}
-puts 
