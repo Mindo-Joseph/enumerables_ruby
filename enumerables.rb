158e17cd2e2ad38622f192a042aa0db6c1d98daf
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-04-14 12:24:21 +0300

    create my_each_with_index

--- enumerables.rb
+++ enumerables.rb
@@ -1,7 +1,12 @@
 module Enumerable
     def my_each
         self.length.times { |i| yield(self[i])}
-        self
+    end
+
+    def my_each_with_index
+       for index in (0..self.length-1)
+            yield(self[index],index)
+       end
     end
 
 
@@ -9,4 +14,11 @@ module Enumerable
 
 end
 my_test = [1,3,"stuff",0]
+
+puts "my_each:"
 my_test.my_each { |i| print i.to_s + " "}
+puts
+
+puts "my_each_with_index:"
+my_test.my_each_with_index { |item,index| print [item,index]}
+puts
